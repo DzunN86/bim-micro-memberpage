b259b65376a3a4b6582c23de381909bcc41101b1
Dzun Nurroin <nurroinz@gmail.com>
2021-02-07 16:30:33 +0700

    Create index.js

---
+++ src/store/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+
+import users from "./users";
+import courses from "./courses";
+import orders from "./orders";
+
+export default combineReducers({
+  users,
+  courses,
+  orders,
+});
