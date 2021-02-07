a0960b1878fc05c9cb3cc30bb47227219a505aa5
Dzun Nurroin <nurroinz@gmail.com>
2021-02-07 16:31:04 +0700

    Create Loading.js

---
+++ src/parts/Loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <section className="h-screen flex justify-center flex-col items-center relative z-50 bg-white">
+      <h1 className="text-3xl text-gray-900 mt-12">Doing science...</h1>
+    </section>
+  );
+}
