b33f1309ac856f9f41dcd669f56a23c043cffb36
Dzun Nurroin <nurroinz@gmail.com>
2021-02-07 16:32:31 +0700

    Create courses.js

---
+++ src/constants/api/courses.js
@@ -0,0 +1,7 @@
+import axios from "configs/axios";
+export default {
+  details: (id) => axios.get(`/courses/${id}`).then((res) => res.data),
+
+  join: (id) => axios.post("/my-courses", { course_id: id }),
+  mine: () => axios.get("/my-courses"),
+};
