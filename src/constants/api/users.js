035ea91babde050f71c80362930b6a77f8957ca9
Dzun Nurroin <nurroinz@gmail.com>
2021-02-07 16:32:22 +0700

    Create users.js

---
+++ src/constants/api/users.js
@@ -0,0 +1,15 @@
+import axios from "configs/axios";
+
+export default {
+  login: (credentials) => axios.post("/users/login", credentials),
+  register: (payload) => axios.post("/users/register", payload),
+  refresh: (credentials) =>
+    axios.post("/refresh-tokens", {
+      refresh_token: credentials.refresh_token,
+      email: credentials.email,
+    }),
+
+  details: () => axios.get("/users"),
+  update: (data) => axios.put("/users", data),
+  logout: () => axios.post("/users/logout"),
+};
